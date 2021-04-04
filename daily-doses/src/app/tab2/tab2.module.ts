73ad5d0f3abaf4b82b3c21bd2f2cf47ec01f3302
Giovannapls <37786977+Giovannapls@users.noreply.github.com>
2021-04-04 18:17:17 -0300

    Ajustes iniciais

--- daily-doses/src/app/tab2/tab2.module.ts
+++ daily-doses/src/app/tab2/tab2.module.ts
@@ -1,4 +1,4 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,8 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @NgModule({
   imports: [
@@ -18,4 +20,22 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule {
+  feriados: Observable<any>;
+  frase: Observable<any>;
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) { 
+    this.frase = this.httpClient.get('https://goquotes-api.herokuapp.com/api/v1/random?count=1');
+    this.feriados = this.httpClient.get('https://calendarific.com/api/v2/holidays?&api_key=80e93351ad0cb3981ddbd6dab3ffea654ddc7f1c&country=BR&year=2021&month=4');
+
+    
+    this.feriados
+    .subscribe(data => {
+      console.log('my data: ', data.response.holidays);
+    })
+
+    this.frase
+    .subscribe(data => {
+      console.log('my data: ', data.quotes[0]);
+    })
+  }
+}
--- daily-doses/src/app/tab3/tab3.module.ts
+++ daily-doses/src/app/tab3/tab3.module.ts
@@ -1,12 +1,13 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { HttpClient } from '@angular/common/http';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { Observable } from 'rxjs';
 
 @NgModule({
   imports: [
@@ -19,4 +20,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule {
+
+}
