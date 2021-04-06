6ce17e5f3eca241ad4551688f2448df321ca9910
DaniSchmidtt <66496761+DaniSchmidtt@users.noreply.github.com>
2021-04-05 21:03:29 -0300

    Ajustes na previsão do tempo

--- daily-doses/src/app/tab1/tab1.module.ts
+++ daily-doses/src/app/tab1/tab1.module.ts
@@ -5,11 +5,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { LOCALE_ID } from '@angular/core';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { fraseAleatoria } from '../models/fraseAleatoria';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
+
 
 @NgModule({
   imports: [
@@ -19,6 +26,7 @@ import { fraseAleatoria } from '../models/fraseAleatoria';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page], 
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}]
 })
 export class Tab1PageModule {}
--- daily-doses/src/app/tab1/tab1.page.ts
+++ daily-doses/src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { covid } from '../models/covid';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-tab1',
@@ -16,28 +17,26 @@ export class Tab1Page {
   covidConfirmed:covid = new covid();
   covidRecovered:covid = new covid();
   covidDeaths:covid = new covid();
-  aday: String = new Date().getDate().toString();
-  amonth: String = new Date().getUTCMonth().toString();
-  ayear: String = new Date().getFullYear().toString();
+  dateformated = moment().subtract(1,'days').format('YYYY-MM-DD');
+  dateformatedBR = moment().subtract(1,'days').format('DD-MM-YYYY');
   constructor(public navCtrl: NavController, public httpClient: HttpClient) {
-    this.covidConfirmedRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/confirmed?from='+this.ayear+'-'+this.amonth+'-'+this.aday+'T00:00:00Z&to='+this.ayear+'-'+this.amonth+'-'+this.aday+'T23:59:59Z')
+    this.covidConfirmedRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/confirmed?from='+this.dateformated+'T00:00:00Z&to='+this.dateformated+'T23:59:59Z')
     this.covidConfirmedRequest
     .subscribe(data => {
       this.covidConfirmed = data[0];
-      console.log('my data: ', this.covidConfirmed);
     })
-    this.covidRecoveredRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/recovered?from='+this.ayear+'-'+this.amonth+'-'+this.aday+'T00:00:00Z&to='+this.ayear+'-'+this.amonth+'-'+this.aday+'T23:59:59Z')
+    this.covidRecoveredRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/recovered?from='+this.dateformated+'T00:00:00Z&to='+this.dateformated+'T23:59:59Z')
     this.covidRecoveredRequest
     .subscribe(data => {
       this.covidRecovered = data[0];
-      console.log('my data: ', this.covidRecovered);
     })
-    this.covidDeathsRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/deaths?from='+this.ayear+'-'+this.amonth+'-'+this.aday+'T00:00:00Z&to='+this.ayear+'-'+this.amonth+'-'+this.aday+'T23:59:59Z')
+    this.covidDeathsRequest = this.httpClient.get('https://api.covid19api.com/total/country/brazil/status/deaths?from='+this.dateformated+'T00:00:00Z&to='+this.dateformated+'T23:59:59Z')
     this.covidDeathsRequest
     .subscribe(data => {
       this.covidDeaths = data[0];
-      console.log('my data: ', this.covidDeaths);
     })
   }
+
+  
 
 }
